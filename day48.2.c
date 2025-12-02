2d284fcfbe2b584cbd6d2328a11811b5f2cd23a6
Arpit-2007 <arpitsingh1132007@gmail.com>
2025-12-02 19:48:15 +0530

    codes of day 48

---
+++ day48.2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void reverse(char str[], int start, int end) {
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[300];
+    int i = 0, start = 0;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0') {
+        if (str[i] == ' ' || str[i] == '\n') {
+            reverse(str, start, i - 1);
+            start = i + 1;
+        }
+        i++;
+    }
+
+    printf("Reversed words: %s", str);
+    return 0;
+}
